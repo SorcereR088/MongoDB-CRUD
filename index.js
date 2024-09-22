f7cc9cfe46965b563f78f39be02218f06cd03795
Sachin Maharjan <sachinmhzn7@gmail.com>
2024-09-22 22:10:17 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const fs = require('fs')
+const path = require("path")
+
+//middleware
+app.set("view engine", "ejs");
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")));
+
+//basic routing
+app.get("/", function(req, res){
+    fs.readdir(`./files`, function(err, files){
+        res.render("index", {files: files});
+    })
+})
+
+app.post('/create', function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/") 
+    })
+})
+
+app.get("/file/:filename", (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render("show", {filename: req.params.filename, filedata:filedata})
+    })
+})
+
+
+app.listen(3000);
