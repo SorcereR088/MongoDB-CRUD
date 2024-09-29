82a1ba0aad9c6e3e8372d534b30c1a9f1a8ed903
Sachin Maharjan <sachinmhzn7@gmail.com>
2024-09-29 18:50:44 +0545

    initial commit

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const fs = require('fs')
-const path = require("path")
+const path = require('path');
+const userModel = require("./models/user.js")
 
-//middleware
 app.set("view engine", "ejs");
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")))
 
-//basic routing
-app.get("/", function(req, res){
-    fs.readdir(`./files`, function(err, files){
-        res.render("index", {files: files});
-    })
+app.get("/", (req, res) => {
+    res.render("index")
 })
 
-app.post('/create', function(req, res){
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
-        res.redirect("/") 
-    })
+app.get("/read", async (req, res) => {
+    let users = await userModel.find();
+    res.render("read", {users});
 })
 
-app.get("/file/:filename", (req, res) => {
-    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
-        res.render("show", {filename: req.params.filename, filedata:filedata})
-    })
+app.get("/delete/:id", async (req, res) => {
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read")
+})
+
+app.post("/create", async (req, res) => {
+    let {name, email, image} = req.body
+   let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    });
+
+    res.send(createdUser);
 })
 
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server Running")
+});
